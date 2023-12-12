acc6ca4ed22cea3c4c38e6416736df006a448d33
ronyhe <ronyheruth@wix.com>
2023-12-13 00:25:04 +0200

    Better ts confgi

--- src/background.ts
+++ src/background.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // A generic onclick callback function.
-/// <reference path="../node_modules/chrome-types/index.d.ts"/>
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'bustIt') {
         console.log('Bust it!')
