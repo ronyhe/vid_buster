030cbb558cfd8d430f57107005966740f4ad47fc
ronyhe <ronyheruth@wix.com>
2023-12-13 17:38:23 +0200

    Remove some options

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,5 @@
 import { Message } from './messages'
 
-await main()
-
 async function main() {
     const videoElements = document.querySelectorAll('video')
     const videos = Array.from(videoElements).map((video) => ({
@@ -11,4 +9,7 @@ async function main() {
     await chrome.runtime.sendMessage(message)
 }
 
-export {}
+main().catch((e) => {
+    console.error(e)
+    throw e
+})
