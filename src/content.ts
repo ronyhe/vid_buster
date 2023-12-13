dd8e82227b953f94d3299f2394aa7eb5429b7e13
ronyhe <ronyheruth@wix.com>
2023-12-13 21:03:36 +0200

    List videos

--- src/content.ts
+++ src/content.ts
@@ -3,18 +3,14 @@ import { tryAsVideoRequestMessage, VideoResponseMessage } from './messages'
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
     const videoRequest = tryAsVideoRequestMessage(message)
     if (videoRequest) {
+        const videos = document.querySelectorAll('video')
+
         const message: VideoResponseMessage = {
             kind: 'videosResponse',
-            videos: [
-                {
-                    title: 'Video 1',
-                    src: 'https://example.com/video1.mp4',
-                },
-                {
-                    title: 'Video 2',
-                    src: 'https://example.com/video2.mp4',
-                },
-            ],
+            videos: Array.from(videos).map(({ src, title }) => ({
+                src,
+                title,
+            })),
         }
         // @ts-ignore
         sendResponse(message)
