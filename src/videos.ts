6933995561b29ceedf1263b0a5f4d1a3ad9e3c9d
ronyhe <ronyheruth@wix.com>
2023-12-14 23:58:20 +0200

    Starting to use underlying lib

--- src/videos.ts
+++ src/videos.ts
@@ -3,6 +3,6 @@ import downloader from 'youtube-dl-exec'
 export async function getVideos(url: string) {
     return await downloader(url, {
         flatPlaylist: true,
-        dumpJson: true,
+        dumpSingleJson: true,
     })
 }
