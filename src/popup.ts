1a4f4d0a0a52fd65700a0468557ba0a673dc2f34
ronyhe <ronyheruth@wix.com>
2023-12-14 20:42:36 +0200

    Easier eyeball sanity checking by showin urls

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,5 @@
 async function checkVideos() {
+    document.querySelector('#vid-buster')?.remove()
     const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
     const tabCount = tabs.length
     if (tabCount === 0) {
@@ -7,8 +8,13 @@ async function checkVideos() {
     if (tabCount > 1) {
         throw new Error('More than one active tab found in current window?')
     }
-    const { url } = tabs[0]
-    console.log(url)
+    const tab = tabs[0]
+    const { url } = tab
+    const text = url ?? 'No URL found'
+    const p = document.createElement('p')
+    p.id = 'vid-buster'
+    p.textContent = text
+    document.body.appendChild(p)
 }
 
 checkVideos().catch((e) => {
