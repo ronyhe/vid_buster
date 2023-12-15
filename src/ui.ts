a8f84f9904e884609c5c8a4d3e33e665fa582389
ronyhe <ronyheruth@wix.com>
2023-12-15 18:35:29 +0200

    Nicer display

--- src/ui.ts
+++ src/ui.ts
@@ -24,9 +24,15 @@ export function showFormats(formats: Format[]) {
     formats.forEach((f) => {
         const a = document.createElement('a')
         a.href = '#'
-        a.textContent = `${f.note} ${f.resolution} ${f.extension} ${f.size}`
+        a.textContent = formatLinkText(f)
         a.title = f.url
         document.body.appendChild(a)
         document.body.appendChild(document.createElement('br'))
     })
+}
+
+function formatLinkText(f: Format): string {
+    return [f.size, f.extension, f.resolution, f.note]
+        .filter((x) => x !== null)
+        .join(' ')
 }
--- src/videos.ts
+++ src/videos.ts
@@ -9,23 +9,19 @@ export async function getFormats(url: string): Promise<Format[]> {
 }
 
 export interface Format {
-    extension: string
-    note: string
-    size: number
+    extension: string | null
+    note: string | null
+    size: string
     resolution: string
     id: string
     url: string
 }
 
-function compareYtFormats(a: YtFormat, b: YtFormat): number {
-    return b.quality - a.quality
-}
-
 function convertFromYtFormat(f: YtFormat): Format {
     return {
-        extension: f.ext,
-        note: f.format_note,
-        size: f.filesize,
+        extension: nullableString(f.ext),
+        note: nullableString(f.format_note),
+        size: fileSizeString(f.filesize),
         resolution: resolution(f),
         id: f.format_id,
         url: f.url,
@@ -36,4 +32,34 @@ function resolution(f: YtFormat): string {
     // @ts-ignore - resolution is not in the type definition, but I saw it in the actual response
     const res: string = f.resolution
     return res ?? `${f.width}x${f.height}`
+}
+
+function nullableString(s: string): string | null {
+    if (s === undefined || s === null) {
+        return null
+    }
+    const text = s.trim()
+    if (!text) {
+        return null
+    }
+    if (text === 'null' || text === 'undefined') {
+        return null
+    }
+    return text
+}
+
+function fileSizeString(sizeInBytes: number): string {
+    if (sizeInBytes === null || sizeInBytes === undefined) {
+        return `?? MB`
+    }
+    const sizeInKb = Math.round(sizeInBytes / 1024)
+    if (sizeInKb < 1024) {
+        return `${sizeInKb} KB`
+    }
+    const sizeInMb = Math.round(sizeInKb / 1024)
+    if (sizeInMb < 1024) {
+        return `${sizeInMb} MB`
+    }
+    const sizeInGb = Math.round(sizeInMb / 1024)
+    return `${sizeInGb} GB`
 }
