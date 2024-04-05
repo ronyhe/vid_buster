fd8c975c29c35700ae87487a75f56acd6722eada
ronyhe <ronyheruth@wix.com>
2024-04-05 19:51:24 +0300

    Setup tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React, { useCallback } from 'react'
+import { Box, Tab, Tabs } from '@mui/material'
+import Formats from './Formats'
+import Downloads from './Downloads'
 
 export default function App() {
+    const [tabValue, setTabValue] = React.useState(0)
+
+    const handleChange = useCallback(
+        (_: React.SyntheticEvent, newValue: number) => {
+            setTabValue(newValue)
+        },
+        [],
+    )
+
     return (
         <Box
             sx={{
                 background: 'red',
             }}
         >
-            <Typography variant="h1">Hello, World!</Typography>
+            <Box sx={{ width: '100%', height: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs
+                        value={tabValue}
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                    >
+                        <Tab label="Here" />
+                        <Tab label="Downloads" />
+                    </Tabs>
+                </Box>
+                {tabValue === 0 ? <Formats /> : <Downloads />}
+            </Box>
         </Box>
     )
 }
---
+++ src/Formats.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Formats() {
+    return <div>Formats</div>
+}
