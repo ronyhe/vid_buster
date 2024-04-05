ea9f7951062840117f3ebdf91af89bb93e599c48
ronyhe <ronyheruth@wix.com>
2024-04-05 19:38:50 +0300

    Start mui rewrite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+export default function App() {
+    return (
+        <Box
+            sx={{
+                background: 'red',
+            }}
+        >
+            <Typography variant="h1">Hello, World!</Typography>
+        </Box>
+    )
+}
