2bf6e01a3dc61596d893ac56439e43c0969a70c5
ronyhe <ronyheruth@wix.com>
2024-08-31 18:04:53 +0300

    Test

--- test/destinationDialog.test.tsx
+++ test/destinationDialog.test.tsx
@@ -48,21 +48,14 @@ test('DestinationDialog', async (t) => {
         correct(onClose.mock.calls[0].arguments[0] === 'no-title')
     })
 
-    await t.test(
-        'Calls onClose with user inputted file name',
-        // I'm unable to implement this test at the moment.
-        // Perhaps I don't understand the tools involved well enough?
-        // I'll come back to it
-        { skip: true },
-        async () => {
-            const onClose = t.mock.fn()
-            const userFileName = 'user-file-name.mp4'
-            const { screen, user } = render(
-                <DestinationDialog open={true} onClose={onClose} />,
-            )
-            await user.keyboard(userFileName)
-            await user.click(screen.getByRole('button', { name: 'Ok' }))
-            correct(onClose.mock.calls[0].arguments[1] === userFileName)
-        },
-    )
+    await t.test('Calls onClose with user inputted file name', async () => {
+        const onClose = t.mock.fn()
+        const userFileName = 'user-file-name.mp4'
+        const { screen, user } = render(
+            <DestinationDialog open={true} onClose={onClose} />,
+        )
+        await user.keyboard(userFileName)
+        await user.click(screen.getByRole('button', { name: 'Ok' }))
+        correct(onClose.mock.calls[0].arguments[0] === userFileName)
+    })
 })
