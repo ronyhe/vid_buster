315bfdff16271d1cb70d8735fe65466e515df28a
ronyhe <ronyheruth@wix.com>
2024-09-06 01:04:51 +0300

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,9 @@ export function mapValues<A, B>(
     obj: { [key: string]: A },
     fn: (value: A, key: string, coll: { [key: string]: A }) => B,
 ): { [key: string]: B } {
-    return Object.fromEntries(
-        Object.entries(obj).map(([key, value]) => [key, fn(value, key, obj)]),
-    )
+    const result: { [key: string]: B } = {}
+    for (const key in obj) {
+        result[key] = fn(obj[key], key, obj)
+    }
+    return result
 }
