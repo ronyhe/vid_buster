c069d769222c23f953af1ddb4a19c679497a4067
ronyhe <ronyheruth@wix.com>
2024-09-06 20:31:18 +0300

    Quote stuff

--- src/server/videos.ts
+++ src/server/videos.ts
@@ -28,7 +28,7 @@ export function downloadVideo(
     downloadDestination: string,
     filename: string,
 ): TerminalStreams {
-    const command = `yt-dlp --no-warnings --newline -f ${formatId} -P ${downloadDestination} -o "${filename}" ${url}`
+    const command = `yt-dlp --no-warnings --newline -f ${formatId} -P "${downloadDestination}" -o "${filename}" "${url}"`
     console.log('command:', command)
     const { stdout, stderr } = child_process.exec(command)
     if (!stdout) {
