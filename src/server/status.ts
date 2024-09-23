e9015339015624607bca0325098754fae1cbba99
ronyhe <ronyheruth@wix.com>
2024-09-24 00:13:35 +0300

    Start tracking improvements

--- src/server/status.ts
+++ src/server/status.ts
@@ -4,32 +4,57 @@ export interface TerminalStreams {
     stderr: ReadLine
 }
 
+interface Status {
+    closed: boolean
+    error: string | null
+    last: string
+}
+
 type Key = string
 
 export class Tracker {
-    private map: Map<Key, string> = new Map()
+    private map: Map<Key, Status> = new Map()
 
     track(key: Key, streams: TerminalStreams) {
-        if (!this.map.has(key)) {
-            this.map.set(key, 'No status yet')
-        }
+        this.map.set(key, {
+            closed: false,
+            error: null,
+            last: ''
+        })
         streams.stderr.on('line', line => {
-            console.error(`Error for ${key}:`)
-            console.error(line)
+            this.update(key, {
+                error: line,
+                closed: true
+            })
         })
         streams.stdout.on('line', line => {
-            this.map.set(key, line)
+            this.update(key, {
+                last: line
+            })
         })
         streams.stdout.on('close', () => {
-            this.map.delete(key)
+            this.update(key, {
+                closed: true
+            })
         })
+    }
+
+    update(key: Key, partialStatus: Partial<Status>) {
+        this.map.set(key, {
+            ...this.map.get(key)!,
+            ...partialStatus
+        })
+    }
+
+    delete(key: Key) {
+        this.map.delete(key)
     }
 
     getReports() {
         return Array.from(this.map.entries())
             .map(([key, status]) => ({
                 title: key,
-                status
+                status: status.last
             }))
             .sort((a, b) =>
                 a.title.localeCompare(b.title, undefined, { numeric: true })
