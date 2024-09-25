6c55495e4e1b6c592e93563947b9002377cc6ddf
ronyhe <ronyheruth@wix.com>
2024-09-25 23:52:10 +0300

    Add more tests

--- test/tracker.test.ts
+++ test/tracker.test.ts
@@ -2,8 +2,8 @@ import { Tracker } from '../src/server/tracking'
 import { test, expect } from './asserts'
 import EventEmitter from 'node:events'
 
-test('Tracker', t => {
-    t.test('tracks the latest status', () => {
+test('Tracker', async t => {
+    await t.test('tracks the latest status', () => {
         const tracker = new Tracker()
         const stdout = new EventEmitter()
         const stderr = new EventEmitter()
@@ -29,6 +29,28 @@ test('Tracker', t => {
         expect(tracker.getStatus()).toEqual([
             expect.objectContaining({
                 lastStatus: line2,
+                ...details
+            })
+        ])
+    })
+
+    await t.test('tracks closing', () => {
+        const tracker = new Tracker()
+        const stdout = new EventEmitter()
+        const stderr = new EventEmitter()
+        const streams = {
+            stdout,
+            stderr
+        }
+        const id = tracker.track('title', streams)
+        const details = {
+            title: 'title',
+            id
+        }
+        stdout.emit('close')
+        expect(tracker.getStatus()).toEqual([
+            expect.objectContaining({
+                closed: true,
                 ...details
             })
         ])
