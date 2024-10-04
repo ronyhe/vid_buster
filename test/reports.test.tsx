9da594cc7af58f0659573854639efd0ec3cc3dc3
ronyhe <ronyheruth@wix.com>
2024-10-04 18:58:47 +0300

    More tests

--- test/reports.test.tsx
+++ test/reports.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { test, expect } from './asserts'
+import { test, expect, expectConnected } from './asserts'
 import { cleanup, render } from './render'
 import { Reports } from '../src/components/Reports'
 
@@ -7,10 +7,24 @@ test('<Reports />', async t => {
     t.afterEach(cleanup)
 
     await t.test('Calls getReports', async () => {
-        const getReports = t.mock.fn(async () => [])
-        render(
+        const getReports = t.mock.fn(async () => [
+            {
+                id: 1,
+                closed: false,
+                error: null,
+                lastStatus: 'active',
+                title: 'Report Title'
+            }
+        ])
+        const { screen } = render(
             <Reports getReports={getReports} interval={0} onDelete={() => {}} />
         )
+        expectConnected(await screen.findByText('Report Title'))
         expect(getReports).toHaveBeenCalledTimes(1)
     })
+
+    await t.test('Calls onDelete', { todo: true })
+    await t.test('Shows loading state', { todo: true })
+    await t.test('Shows empty state', { todo: true })
+    await t.test('Shows error state', { todo: true })
 })
