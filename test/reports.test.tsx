b448e13c2f055f813970d2c54950a8ad86e4ae39
ronyhe <ronyheruth@wix.com>
2024-10-04 19:04:16 +0300

    More tests

--- test/reports.test.tsx
+++ test/reports.test.tsx
@@ -23,7 +23,26 @@ test('<Reports />', async t => {
         expect(getReports).toHaveBeenCalledTimes(1)
     })
 
-    await t.test('Calls onDelete', { todo: true })
+    await t.test('Calls onDelete', async t => {
+        const getReports = t.mock.fn(async () => [
+            {
+                id: 1,
+                closed: false,
+                error: null,
+                lastStatus: 'active',
+                title: 'Report Title'
+            }
+        ])
+        const onDelete = t.mock.fn()
+        const { screen, user } = render(
+            <Reports getReports={getReports} interval={0} onDelete={onDelete} />
+        )
+        const button = await screen.findByRole('button')
+        await user.click(button)
+        expect(onDelete).toHaveBeenCalledWith(1)
+        expect(onDelete).toHaveBeenCalledTimes(1)
+    })
+
     await t.test('Shows loading state', { todo: true })
     await t.test('Shows empty state', { todo: true })
     await t.test('Shows error state', { todo: true })
